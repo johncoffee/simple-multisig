e69a0c289dbdb12a36050b8a72558b6495f9243f
johncoffee <github2017@ragelse.dk>
2018-10-15 09:59:43 +0200

    split all requires into single expressions and added debug messages

--- contracts/SimpleMultiSig.sol
+++ contracts/SimpleMultiSig.sol
@@ -9,11 +9,13 @@ contract SimpleMultiSig {
 
   // Note that owners_ must be strictly increasing, in order to prevent duplicates
   constructor(uint threshold_, address[] owners_) public {
-    require(owners_.length <= 10 && threshold_ <= owners_.length && threshold_ >= 0);
+    require(owners_.length <= 10, "owners must be less than 10");
+    require(threshold_ <= owners_.length, "threshold must not be greater than number of owners");
+    require(threshold_ >= 0, "thresholds has to be or larger");
 
-    address lastAdd = address(0); 
+    address lastAdd = address(0);
     for (uint i = 0; i < owners_.length; i++) {
-      require(owners_[i] > lastAdd);
+      require(owners_[i] > lastAdd, "problem with order of owners");
       isOwner[owners_[i]] = true;
       lastAdd = owners_[i];
     }
@@ -23,8 +25,8 @@ contract SimpleMultiSig {
 
   // Note that address recovered from signatures must be strictly increasing, in order to prevent duplicates
   function execute(uint8[] sigV, bytes32[] sigR, bytes32[] sigS, address destination, uint value, bytes data) public {
-    require(sigR.length == threshold);
-    require(sigR.length == sigS.length && sigR.length == sigV.length);
+    require(sigR.length == threshold, "number of input signatures doesn't match threshold");
+    require(sigR.length == sigS.length && sigR.length == sigV.length, "lengths of V, R, S is not equal");
 
     // Follows ERC191 signature scheme: https://github.com/ethereum/EIPs/issues/191
     bytes32 txHash = keccak256(byte(0x19), byte(0), this, destination, value, data, nonce);
@@ -32,7 +34,8 @@ contract SimpleMultiSig {
     address lastAdd = address(0); // cannot have address(0) as an owner
     for (uint i = 0; i < threshold; i++) {
       address recovered = ecrecover(txHash, sigV[i], sigR[i], sigS[i]);
-      require(recovered > lastAdd && isOwner[recovered]);
+      require(recovered > lastAdd, "problem with order of signers");
+      require(isOwner[recovered], "recovered address should be owner");
       lastAdd = recovered;
     }
 
@@ -42,7 +45,7 @@ contract SimpleMultiSig {
     nonce = nonce + 1;
     bool success = false;
     assembly { success := call(gas, destination, value, add(data, 0x20), mload(data), 0, 0) }
-    require(success);
+    require(success, "calling destination method failed, for unknown reasons");
   }
 
   function () payable public {}
