77296b32f852014e5a3bed7c610f633e34ff3bbe
johncoffee <github2017@ragelse.dk>
2018-10-24 11:04:41 +0200

    truffle config

--- truffle.js
+++ truffle.js
@@ -1,5 +1,3 @@
-const HDWalletProvider = require("truffle-hdwallet-provider")
-
 module.exports = {
   networks: {
     development: {
@@ -7,14 +5,5 @@ module.exports = {
       port: 7545,
       network_id: "*" // Match any network id
     },
-    rinkeby: {
-      host: "127.0.0.1",
-      port: 8545,
-      network_id: 4, // 1,2, 42, 1337, * (Match any network id)
-    },
-    rinkfura: {
-      provider: new HDWalletProvider("candy maple cake sugar pudding cream honey rich smooth crumble sweet treat", "https://rinkeby.infura.io/v3/"),
-      network_id: 4
-    }
   }
 }
